068086eefb8578040e18193e6d66c14aa241b5ba
TheRenanOfc <162510870+TheRenanOfc@users.noreply.github.com>
2025-10-17 14:29:54 -0300

    Update github.js

--- api/auth/github.js
+++ api/auth/github.js
@@ -1,24 +1,11 @@
-// api/auth/github.js
+// /api/github.js
+import { URLSearchParams } from 'url';
+
 export default function handler(req, res) {
-  try {
-    const client_id = process.env.GITHUB_CLIENT_ID;
-    const baseUrl = process.env.BASE_URL;
-
-    if (!client_id || !baseUrl) {
-      return res.status(500).json({ error: 'Client ID ou BASE_URL não configurado' });
-    }
-
-    const redirect_uri = `${baseUrl}/api/auth/callback`;
-    const scope = 'read:user';
-
-    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(
-      redirect_uri
-    )}&scope=${encodeURIComponent(scope)}`;
-
-    // redireciona pro GitHub
-    res.writeHead(302, { Location: githubAuthUrl });
-    res.end();
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const params = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID,
+    scope: 'read:user repo'
+  });
+  const githubUrl = `https://github.com/login/oauth/authorize?${params.toString()}`;
+  res.redirect(githubUrl);
 }
